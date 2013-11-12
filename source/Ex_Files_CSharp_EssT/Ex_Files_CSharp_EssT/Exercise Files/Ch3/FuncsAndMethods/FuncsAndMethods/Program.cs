b7f1e32738d39ae849e751d095f9696477f03c06
Liz <e.dinunzio@gmail.com>
2013-11-12 16:04:39 -0500

    Using loops, using functions and methods

--- source/Ex_Files_CSharp_EssT/Ex_Files_CSharp_EssT/Exercise Files/Ch3/FuncsAndMethods/FuncsAndMethods/Program.cs
+++ source/Ex_Files_CSharp_EssT/Ex_Files_CSharp_EssT/Exercise Files/Ch3/FuncsAndMethods/FuncsAndMethods/Program.cs
@@ -9,9 +9,18 @@ namespace FuncsAndMethods
     {
         static void Main(string[] args)
         {
+            int result1;
+            int arg1 = 25;
+
+            result1 = formula(arg1);
+            Console.WriteLine("The result is: {0}", result1);
 
             Console.ReadLine();
         }
 
+        static int formula(int theVal)
+        {
+            return (theVal * 2) / 3 + 15;
+        }
     }
 }
--- source/Ex_Files_CSharp_EssT/Ex_Files_CSharp_EssT/Exercise Files/Ch3/Loops/Loops/Program.cs
+++ source/Ex_Files_CSharp_EssT/Ex_Files_CSharp_EssT/Exercise Files/Ch3/Loops/Loops/Program.cs
@@ -9,6 +9,20 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            int myVal = 20;
+
+            // using the continue and break keywords
+            Console.WriteLine("Using break and continue :");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                    continue; // skip the rest of the loop
+
+                if (i == 7)
+                    break;
+
+                Console.WriteLine("i is currently {0}", i);
+            }
 
             Console.ReadLine();
         }
