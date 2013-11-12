e211acdf40d2c78716572d223dd588ae6829132c
Liz <e.dinunzio@gmail.com>
2013-11-12 17:49:40 -0500

    Chapter 4 Variable Scope

--- source/Ex_Files_CSharp_EssT/Ex_Files_CSharp_EssT/Exercise Files/Ch4/VariableScope/VariableScope/Program.cs
+++ source/Ex_Files_CSharp_EssT/Ex_Files_CSharp_EssT/Exercise Files/Ch4/VariableScope/VariableScope/Program.cs
@@ -9,7 +9,14 @@ namespace VariableScope
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 0; i < 10; i++)
+            {
+                int var1 = 20;
+                Console.WriteLine("The value of var1 at pass {0} is {1} ", i, var1);
+            }
+
+            //Console.WriteLine("{0}", var1);
+
             Console.ReadLine();
         }
     }
