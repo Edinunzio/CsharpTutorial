e2e6f857248babf6b9a4872aadcb666caf76b00a
Liz <e.dinunzio@gmail.com>
2013-11-12 16:37:50 -0500

    Chapter 4 Double vs Decimal

--- source/Ex_Files_CSharp_EssT/Ex_Files_CSharp_EssT/Exercise Files/Ch4/DoubleVsDecimal/DoubleVsDecimal/Program.cs
+++ source/Ex_Files_CSharp_EssT/Ex_Files_CSharp_EssT/Exercise Files/Ch4/DoubleVsDecimal/DoubleVsDecimal/Program.cs
@@ -9,6 +9,15 @@ namespace DoubleVsDecimal
     {
         static void Main(string[] args)
         {
+            double twentieth = 0.2f;
+            double one = 1.0f;
+
+            Console.WriteLine("{0}", one - twentieth);
+
+            decimal onetwentieth = 0.2m;
+            decimal realone = 1.0m;
+
+            Console.WriteLine("{0}", realone - onetwentieth);
 
             Console.ReadLine();
         }
